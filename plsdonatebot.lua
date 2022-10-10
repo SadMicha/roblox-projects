7b356626e704753a04e246375d24972d8977c631
SadMicha <55989084+SadMicha@users.noreply.github.com>
2022-10-10 11:11:04 +0200

    Add files via upload

---
+++ plsdonatebot.lua
@@ -0,0 +1,30 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local getgenv = getgenv
+local url = getgenv().url
+if not url then
+    repeat
+        task.wait(10)
+    until url
+    url = getgenv().url
+end
+
+local local_player = game:GetService("Players").LocalPlayer
+
+local data = {
+    ["content"] = "👍 " .. local_player.Name,
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+local headers = {
+    ["content-type"] = "application/json"
+}
+
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+
+task.spawn(function()
+    request(abcdef)
+    task.wait(30)
+end)
